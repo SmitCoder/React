221917ed7aa47839eeb04016547b9390422a7e95
Smit <smitvd2002@gmail.com>
2023-05-30 16:27:49 +0530

    Created a project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import react from "react";
+import ReactDOM from "react-dom";
+
+function Message() {
+  return <h1>hello world</h1>;
+}
+ReactDOM.render(<Message />, document.getElementById("root"));
