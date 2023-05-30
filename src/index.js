515b06a7dce0d2856441553055809d19132e662b
Smit <smitvd2002@gmail.com>
2023-05-30 16:37:57 +0530

    Created a nested component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import react from "react";
 import ReactDOM from "react-dom";
 
 function Message() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <h1>hello world</h1>
+      <Nested />
+    </>
+  );
 }
+
+const Nested = () => {
+  return <h1>This is Nested Component</h1>;
+};
 ReactDOM.render(<Message />, document.getElementById("root"));
