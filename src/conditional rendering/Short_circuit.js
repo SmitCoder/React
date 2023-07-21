67ed8a0fff69048ce519942f531c760f94928a91
Smit <smitvd2002@gmail.com>
2023-07-21 23:26:22 +0530

    ternary operator

--- src/conditional rendering/Short_circuit.js
+++ src/conditional rendering/Short_circuit.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Short_circuit = () => {
   const [text, settext] = useState("");
+  const [error, seterror] = useState(false);
   const firstvalue = text || "hello world";
   const sv = text && "hello karan";
   return (
@@ -9,8 +10,13 @@ const Short_circuit = () => {
       <div className="container">
         {/* <h1>{firstvalue}</h1> */}
         {/* <h2>value: {sv}</h2> */}
-        {text || <h1>"mari value"</h1>}
-        {!text && <h1>"tari value"</h1>}
+        {/* {text || <h1>"mari value"</h1>} */}
+        {/* {!text && <h1>"tari value"</h1>} */}
+        {error}
+        <button className="btn" onClick={() => seterror(!error)}>
+          Toggle
+        </button>
+        {error ? <h1>Error...</h1> : <h1>there is no error</h1>}
       </div>
     </>
   );
