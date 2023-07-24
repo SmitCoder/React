cee3d8d1313055e58fdd458a4ef0881d8246deb2
Smit <smitvd2002@gmail.com>
2023-07-24 18:11:14 +0530

    usereducer refactoring

--- src/index.js
+++ src/index.js
@@ -6,17 +6,8 @@ import "./index.css";
 import Usestate from "./Usestate";
 import UseArray from "./UseArray";
 import Useobject from "./Useobject";
-import Simplecounter from "./Simplecounter";
-import Useeffect_basic from "./useeffect hook/Useeffect_basic";
-import Useeffect_cleanup from "./useeffect hook/Useeffect_cleanup";
-import Useeffect_fetchdata from "./useeffect hook/Useeffect_fetchdata";
-import Multiple_returns from "./useeffect hook/Multiple_returns";
-import Short_circuit from "./conditional rendering/Short_circuit";
-import Show_hide from "./conditional rendering/Show_hide";
-import Basic from "./Forms/Basic";
-import Multiple_inputs from "./Forms/Multiple_inputs";
-import Useref from "./useref/Useref";
 import Userefactor_usestate from "./useref/Userefactor_usestate";
+import Usereducer_refactor from "./useref/Usereducer_refactor";
 
 function Booklist() {
   return (
@@ -28,6 +19,7 @@ function Booklist() {
     </section>
   );
 }
-<Useref />;
+
 <Userefactor_usestate />;
-ReactDOM.render(<Userefactor_usestate />, document.getElementById("root"));
+<Usereducer_refactor />;
+ReactDOM.render(<Usereducer_refactor />, document.getElementById("root"));
---
+++ src/useref/Usereducer_refactor.js
@@ -0,0 +1,63 @@
+import React, { useReducer, useState } from "react";
+import data from "../data";
+import Usestate from "../Usestate";
+
+const reducer = (state, action) => {
+  if (action.type === "ADD_ITEMS") {
+    const newPeople = [...state.people, action.payload];
+    return { ...state, people: newPeople, modelcontent: "item added" };
+  } else {
+    if (action.type === "NOTHING") {
+      console.log(state, action);
+      return { ...state, modelcontent: "please enter a nsme" };
+    }
+  }
+};
+const defaultstate = {
+  people: [],
+  modelcontent: "hello world",
+};
+const Userefactor_usestate = () => {
+  const [name, setname] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultstate);
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newpeople = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEMS", payload: newpeople });
+      setname("");
+    } else {
+      dispatch({ type: "NOTHING" });
+    }
+  };
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handlesubmit}>
+          <div className="form-control">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => {
+                setname(e.target.value);
+              }}
+            />
+          </div>
+          <button type="submit">Add people</button>
+        </form>
+        {state.people.map((users) => {
+          const { id, name } = users;
+          return (
+            <div key={id}>
+              <h4>{name}</h4>
+            </div>
+          );
+        })}
+      </article>
+    </>
+  );
+};
+
+export default Userefactor_usestate;
